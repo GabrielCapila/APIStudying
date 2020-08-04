59f037e715143e00883c1543e3e6e05b1921eff8
Gabriel <gabriel.capila20@gmail.com>
2020-08-03 22:38:07 -0300

    second commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Home from './pages/Home';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import CadastroVideo from './pages/cadastro/Video/index'
+import CadastroCategoria from './pages/cadastro/Categoria';
+
+
+const Pag404 = () => (<div>Página 404</div>)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    <Switch>
+      <Route path="/" exact component={Home}  />
+      <Route path="/cadastro/video" component={CadastroVideo}/>
+      <Route path="/cadastro/categoria" component={CadastroCategoria}/>
+      <Route component={Pag404}  />
+    <Home />
+    </Switch>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
---
+++ src/pages/Home/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import Menu from '../../components/Menu';
+import BannerMain from '../../components/BannerMain'
+import Carousel from '../../components/Carousel'
+import Footer from '../../components/Footer'
+import dadosIniciais from '../../data/dados_iniciais.json'
+
+function Home() {
+  return (
+    <div style={{backgroundColor: '#141414'}} >
+    <Menu/>
+    <BannerMain videoTitle={dadosIniciais.categorias[0].titulo}
+    url={dadosIniciais.categorias[0].videos[0].url}
+    videoDescription={"nao sei"}
+    ></BannerMain>
+    <Carousel
+      ignoreFirstVideo
+      category={dadosIniciais.categorias[0]}
+    ></Carousel>
+    <Carousel
+      category={dadosIniciais.categorias[1]}
+    ></Carousel>
+    <Carousel
+      category={dadosIniciais.categorias[2]}
+    ></Carousel>
+    <Carousel
+      category={dadosIniciais.categorias[3]}
+    ></Carousel>
+    <Carousel
+      category={dadosIniciais.categorias[4]}
+    ></Carousel>
+    <Carousel
+      category={dadosIniciais.categorias[5]}
+    ></Carousel>
+    
+    <Footer></Footer>
+    </div>
+  );
+}
+
+export default Home;
